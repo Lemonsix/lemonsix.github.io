bb940676ce194f4388c744e44dad1a44d55de9e1
Lemonsix <alan.g.asc@gmail.com>
2023-06-11 14:41:28 -0300

    asd

--- svelte.config.js
+++ svelte.config.js
@@ -1,22 +1,14 @@
-import pkg from './package.json' assert { type: 'json' };
 import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/kit/vite';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-    kit: {
-        adapter: adapter({
-            pages: 'build',
-            assets: 'build',
-			fallback: '200.html' // fallback page
-        }),
-    },
-    vite: {
-        ssr: {
-            noExternal: Object.keys(pkg.dependencies || {})
-        }
-    },
-    preprocess: vitePreprocess()
+	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
+	// for more information about preprocessors
+	kit: {
+		adapter: adapter()
+	},
+	preprocess: vitePreprocess()
 };
 
 export default config;
