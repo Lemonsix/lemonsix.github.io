ccb21978305caf5f19b53645dd132cb169ce57b6
Lemonsix <alan.g.asc@gmail.com>
2023-06-11 13:38:36 -0300

    push

--- svelte.config.js
+++ svelte.config.js
@@ -1,21 +1,22 @@
+import pkg from './package.json' assert { type: 'json' };
 import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/kit/vite';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
-	// for more information about preprocessors
-	kit: {
-		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
-		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
-		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		adapter: adapter({
-			pages: 'build', 
-			assets: 'build', 
-			fallback: '200.html' // it can be any file you want, like index.html
-		  })
-	},
-	preprocess: vitePreprocess()
+    kit: {
+        adapter: adapter({
+            pages: 'build',
+            assets: 'build',
+			fallback: '200.html' // fallback page
+        }),
+    },
+    vite: {
+        ssr: {
+            noExternal: Object.keys(pkg.dependencies || {})
+        }
+    },
+    preprocess: vitePreprocess()
 };
 
 export default config;
